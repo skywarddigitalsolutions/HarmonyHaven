a9b342df5052cb0c0ee7346def88a17dd9093bdc
Matias Ezequiel Roel <roel@fktech.net>
2024-08-20 20:33:02 -0300

    FIX: navbar mobile dropdown

--- app/navbar/page.tsx
+++ app/navbar/page.tsx
@@ -1,6 +1,6 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
+import { useState } from "react";
 import {
   Dialog,
   DialogPanel,
@@ -11,7 +11,7 @@ import {
   PopoverButton,
   PopoverGroup,
   PopoverPanel,
-} from '@headlessui/react'
+} from "@headlessui/react";
 import {
   ArrowPathIcon,
   Bars3Icon,
@@ -20,29 +20,61 @@ import {
   FingerPrintIcon,
   SquaresPlusIcon,
   XMarkIcon,
-} from '@heroicons/react/24/outline'
-import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
+} from "@heroicons/react/24/outline";
+import {
+  ChevronDownIcon,
+  PhoneIcon,
+  PlayCircleIcon,
+} from "@heroicons/react/20/solid";
 import { TbMassage } from "react-icons/tb";
 import { GiBarefoot } from "react-icons/gi";
 import { GiFootprint } from "react-icons/gi";
 import { GiJoint } from "react-icons/gi";
 import { MdAssistWalker } from "react-icons/md";
 
-
 const services = [
-  { name: 'Masajes', description: 'Bienestar corporal', href: '/servicios/masajes', icon: TbMassage  },
-  { name: 'Podología', description: 'Cuidado y alivio para pies y uñas', href: '/servicios/podologia', icon: GiFootprint},
-  { name: 'Reflexología', description: 'Estimulación de puntos en los pies para equilibrar el cuerpo', href: '/servicios/reflexologia', icon: GiBarefoot },
-  { name: 'Rehabilitación', description: 'Recuperación de funciones físicas mediante ejercicios y terapias', href: '/servicios/rehabilitacion', icon: MdAssistWalker  },
-  { name: 'Traumatología', description: 'Tratamiento de lesiones', href: '/servicios/traumatologia', icon: GiJoint  },
-]
+  {
+    name: "Masajes",
+    description: "Bienestar corporal",
+    href: "/servicios/masajes",
+    icon: TbMassage,
+  },
+  {
+    name: "Podología",
+    description: "Cuidado y alivio para pies y uñas",
+    href: "/servicios/podologia",
+    icon: GiFootprint,
+  },
+  {
+    name: "Reflexología",
+    description: "Estimulación de puntos en los pies para equilibrar el cuerpo",
+    href: "/servicios/reflexologia",
+    icon: GiBarefoot,
+  },
+  {
+    name: "Rehabilitación",
+    description:
+      "Recuperación de funciones físicas mediante ejercicios y terapias",
+    href: "/servicios/rehabilitacion",
+    icon: MdAssistWalker,
+  },
+  {
+    name: "Traumatología",
+    description: "Tratamiento de lesiones",
+    href: "/servicios/traumatologia",
+    icon: GiJoint,
+  },
+];
 
 export default function Navbar() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
     <header className="bg-verde fixed top-0 inset-x-0 z-50">
-      <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-3 lg:px-8">
+      <nav
+        aria-label="Global"
+        className="mx-auto flex max-w-7xl items-center justify-between p-3 lg:px-8"
+      >
         <div className="flex lg:flex-1">
           <a href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Harmony Haven</span>
@@ -60,18 +92,19 @@ export default function Navbar() {
           </button>
         </div>
         <PopoverGroup className="hidden lg:flex lg:gap-x-12">
-        <a href="/" className="text-sm leading-6 text-white">
+          <a href="/" className="text-sm leading-6 text-white">
             Inicio
           </a>
           <Popover className="relative">
             <PopoverButton className="flex items-center gap-x-1 text-sm leading-6 text-white">
               Servicios
-              <ChevronDownIcon aria-hidden="true" className="h-5 w-5 flex-none text-white" />
+              <ChevronDownIcon
+                aria-hidden="true"
+                className="h-5 w-5 flex-none text-white"
+              />
             </PopoverButton>
 
-            <PopoverPanel
-              className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-verde shadow-lg ring-1 ring-gray-900/5 transition data-[closed]:translate-y-1 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 data-[enter]:ease-out data-[leave]:ease-in"
-            >
+            <PopoverPanel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-verde shadow-lg ring-1 ring-gray-900/5 transition data-[closed]:translate-y-1 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 data-[enter]:ease-out data-[leave]:ease-in">
               <div className="p-4">
                 {services.map((item) => (
                   <div
@@ -79,15 +112,23 @@ export default function Navbar() {
                     className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
                   >
                     <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-verdeclaro">
-                      <item.icon aria-hidden="true" className="h-6 w-6 text-naranja group-hover:text-verde" />
+                      <item.icon
+                        aria-hidden="true"
+                        className="h-6 w-6 text-naranja group-hover:text-verde"
+                      />
                     </div>
                     <div className="flex-auto">
-                      <a href={item.href} className="block text-white hover:text-verde">
+                      <a
+                        href={item.href}
+                        className="block text-white hover:text-verde"
+                      >
                         {item.name}
                         <span className="absolute inset-0" />
                       </a>
-                      <div className='group'>
-                      <p className="mt-1 text-gray-300 group-hover:text-verdeoscuro">{item.description}</p>
+                      <div className="group">
+                        <p className="mt-1 text-gray-300 group-hover:text-verdeoscuro">
+                          {item.description}
+                        </p>
                       </div>
                     </div>
                   </div>
@@ -106,19 +147,18 @@ export default function Navbar() {
             Contacto
           </a>
         </PopoverGroup>
-       
       </nav>
-      <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+      <Dialog
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+        className="lg:hidden"
+      >
         <div className="fixed inset-0 z-10" />
         <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-verde px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Harmony Haven</span>
-              <img
-                alt=""
-                src="./logo.svg"
-                className="h-10 w-auto"
-              />
+              <img alt="" src="./logo.svg" className="h-10 w-auto" />
             </a>
             <button
               type="button"
@@ -132,24 +172,37 @@ export default function Navbar() {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
-              <a
+                <a
                   href="/"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base leading-7 text-white hover:bg-gray-50"
                 >
-                  Inicio 
+                  Inicio
                 </a>
                 <Disclosure as="div" className="-mx-3">
-                  <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base leading-7 text-white hover:bg-verdeclaro">
+                  <Disclosure.Button className="group flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base leading-7 text-white hover:bg-verdeclaro">
                     Servicios
-                    <ChevronDownIcon aria-hidden="true" className="h-5 w-5 flex-none group-data-[open]:rotate-180" />
-                  </DisclosureButton>
-                  
+                    <ChevronDownIcon
+                      aria-hidden="true"
+                      className="h-5 w-5 flex-none group-data-[open]:rotate-180"
+                    />
+                  </Disclosure.Button>
+                  <Disclosure.Panel className="space-y-2 ml-10">
+                    {services.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className="-mx-3 block rounded-lg px-3 py-2 text-base leading-7 text-white hover:bg-gray-50"
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </Disclosure.Panel>
                 </Disclosure>
                 <a
                   href="/insumos"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base leading-7 text-white hover:bg-verdeclaro"
                 >
-                  Insumos ortopédicos 
+                  Insumos ortopédicos
                 </a>
                 <a
                   href="/nosotros"
@@ -164,11 +217,10 @@ export default function Navbar() {
                   Contacto
                 </a>
               </div>
-              
             </div>
           </div>
         </DialogPanel>
       </Dialog>
     </header>
-  )
+  );
 }
